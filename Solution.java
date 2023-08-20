a662c21d5903d2953f298d0dac5bbef9d8945e32
Miguel S Peña <113079863+PenaSMiguel92@users.noreply.github.com>
2023-08-19 19:36:15 -0700

    faster implementation

--- Solution.java
+++ Solution.java
@@ -1,42 +1,21 @@
-import java.util.List;
-import java.util.ArrayList;
-
 public class Solution {
     public ListNode deleteDuplicates(ListNode head) {
         if (head == null || head.next == null)
             return head;
+        ListNode answer = new ListNode(-1, head);
+        ListNode current = answer;
 
-        List<Integer> duplicates = generateDuplicatesList(head);
-        System.gc();
-        ListNode ptr1 = head;
-        ListNode ptr2 = new ListNode(-1, head);
-        ListNode prevNode = ptr2;
+        while (current.next != null && current.next.next != null) {
+            if (current.next.val == current.next.next.val) {
+                int duplicate = current.next.val;
+                while (current.next != null && current.next.val == duplicate) {
+                    current.next = current.next.next;
+                }
+            } else {
+                current = current.next;
+            }
+        }
 
-        while (ptr1 != null) {
-            System.out.println(ptr1.val + ":" + duplicates.contains(ptr1.val));
-            if (!duplicates.contains(ptr1.val)) {
-                prevNode.next = ptr1;
-                prevNode = prevNode.next;
-            }
-            ptr1 = ptr1.next;
-        }
-        prevNode.next = null;
-        return ptr2.next;
-    }
-    
-    public List<Integer> generateDuplicatesList(ListNode head) {
-        List<Integer> uniqueList = new ArrayList<>();
-        List<Integer> duplicateList = new ArrayList<>();
-        while (head != null) {
-            if (!uniqueList.contains(head.val)) {
-                uniqueList.add(head.val);
-            } else if (uniqueList.contains(head.val) && !duplicateList.contains(head.val)) {
-                duplicateList.add(head.val);
-            }
-            head = head.next;
-        }
-        System.out.println("unique: " + uniqueList.toString());
-        System.out.println("duplicate: " + duplicateList.toString());
-        return duplicateList;
+        return answer.next;
     }
 }
